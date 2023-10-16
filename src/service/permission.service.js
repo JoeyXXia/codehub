119eaca30d3e77c0a199159ef438f4a55bb82340
夏 <whitenight2xia@gmail.com>
2023-10-16 09:14:10 +0800

    refract check moment

--- src/service/permission.service.js
+++ src/service/permission.service.js
@@ -7,6 +7,16 @@ class PermissionService {
 
         return !!result.length
     }
+
+    async checkResource(resourceName, resourceId, userId) {
+        const statement = `SELECT * FROM ${resourceName} WHERE id = ? AND user_id = ?;`
+        const [result] = await connection.execute(statement, [
+            resourceId,
+            userId
+        ])
+
+        return !!result.length
+    }
 }
 
 module.exports = new PermissionService()
