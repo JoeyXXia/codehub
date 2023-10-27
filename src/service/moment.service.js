f1f4a28ae65c46c0b66b4f70097f13d34deb2ae0
夏 <whitenight2xia@gmail.com>
2023-10-27 11:29:11 +0800

    feat moment detail

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -21,10 +21,31 @@ class MomentService {
     }
 
     async queryById(id) {
-        const statement = `SELECT 
-        m.id id, m.content content,m.createAt createTime, m.updateAt updateTime,
-        JSON_OBJECT('id',u.id,'name',u.name,'createTime', u.createAt,'updateTime',u.updateAt) user
-        FROM moment m LEFT JOIN user u ON u.id = m.user_id where m.id = ?;`
+        const statement = `SELECT
+        m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
+        JSON_OBJECT('id',u.id,'name',u.name,'createTime', u.createAt,'updateTime',u.updateAt) user,
+        (
+            SELECT 
+            JSON_ARRAYAGG(JSON_OBJECT(
+            'id',c.id, 'content',c.content,'commentId',c.comment_id,
+            'user',JSON_OBJECT('id',cu.id,'name',cu.name)
+            ))
+         FROM comment c 
+            LEFT JOIN user cu ON c.user_id = cu.id
+            WHERE c.comment_id = m.id
+        ) comments,
+        ( 
+        JSON_ARRAYAGG(JSON_OBJECT(
+        'id',l.id,'name',l.name
+        ))
+        ) labels
+        FROM moment m
+        LEFT JOIN user u ON u.id = m.user_id
+        LEFT JOIN moment_label ml ON ml.moment_id = m.id
+        LEFT JOIN label l ON ml.label_id = l.id
+        
+        WHERE m.id = ?
+        GROUP BY m.id`
         const [result] = await connection.execute(statement, [id])
         return result
     }
