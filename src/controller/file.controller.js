decb9338b75af34200bf067a8a0d9ff41838ab41
夏 <whitenight2xia@gmail.com>
2023-10-30 10:10:48 +0800

    feat upload avatar

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -1,5 +1,18 @@
+const fileService = require('../service/file.service')
+
 class FileController {
-    async create(ctx, next) {}
+    async create(ctx, next) {
+        const { filename, mimetype, size } = ctx.request.file
+        const { id } = ctx.user
+
+        const result = await fileService.create(filename, mimetype, size, id)
+
+        ctx.body = {
+            code: 0,
+            message: 'upload success',
+            data: result
+        }
+    }
 }
 
 module.exports = new FileController()
--- src/service/file.service.js
+++ src/service/file.service.js
@@ -1,9 +1,15 @@
 const connection = require('../app/dababase')
 
 class FileService {
-    async create(id) {
-        const statement = 'statement'
-        const [result] = await connection.execute(statement, id)
+    async create(filename, mimetype, size, userId) {
+        const statement =
+            'INSERT INTO avatar (filename,mimetype,size,user_id) VALUES (?,?,?,?);'
+        const [result] = await connection.execute(statement, [
+            filename,
+            mimetype,
+            size,
+            userId
+        ])
         return result
     }
 }
